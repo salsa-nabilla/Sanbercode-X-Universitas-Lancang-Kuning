11e2b22e0715884ace50cf4a9adcc33fc5b7eff9
salsa-nabilla <sisalsanabilla@gmail.com>
2024-05-18 13:23:52 +0700

    Tugas-8: OOP-PHP

---
+++ OOP-PHP/animal.php
@@ -0,0 +1,13 @@
+<?php
+class Animal {
+    public $name;
+    public $legs;
+    public $cold_blooded;
+
+    public function __construct($name) {
+        $this->name = $name;
+        $this->legs = 4;
+        $this->cold_blooded = "no";
+    }
+}
+?>
